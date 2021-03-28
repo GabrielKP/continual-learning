dc95dc6ab3d080a9bf80e4fa48db7fe320d21ad3
Gabriel Kressin <gabriel.kressin@fu-berlin.de>
2021-03-28 19:59:32 -0400

    padded sequence, extended train stimuli

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from torch import nn
 from torch import optim
 from torch.utils.data import DataLoader
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device( 'cuda' if torch.cuda.is_available() else 'cpu' )
 
 
 class SequenceDataset( Dataset ):
@@ -36,69 +36,69 @@ class SequenceDataset( Dataset ):
 
 class SequenceClassificationModel(nn.Module):
 
-    def __init__(self, stimuli_dim, hidden_dim, emb_dim):
+    def __init__(self, input_dim, hidden_dim, embedding_dim):
         super(SequenceClassificationModel, self).__init__()
-        self.emb = nn.EmbeddingBag(stimuli_dim, emb_dim)
-        #self.lstm = nn.LSTM( emb_dim, hidden_dim )
-        self.lin = nn.Linear( emb_dim, 1 )
+        self.embed = nn.Embedding( input_dim, embedding_dim )
+        self.lstm = nn.LSTM( embedding_dim, hidden_dim )
+        self.lin = nn.Linear( hidden_dim, 1 )
         self.act = nn.Sigmoid()
 
-    def forward(self, seqs, offsets):
-        embedded = self.emb( seqs, offsets )
-        return  self.act(self.lin( embedded ) )
+    def forward(self, seqs):
+        lengths = [ len( seq ) for seq in seqs ]
+        padded_seqs = nn.utils.rnn.pad_sequence( seqs, batch_first=True )
+        padded_embeds = self.embed( padded_seqs )
+        padded_embeds_packed = nn.utils.rnn.pack_padded_sequence( padded_embeds, lengths, batch_first=True, enforce_sorted=False )
+        _, (hidden, _) = self.lstm( padded_embeds_packed )
+        return  self.act( self.lin( hidden[-1] ) )
 
 
-def collate_batch( batch ):
+def collate_batch(batch):
     """
     https://pytorch.org/tutorials/beginner/text_sentiment_ngrams_tutorial.html
     Basically:
     1. create tensor for all labels in a batch
-    2. smash all sequences together in one big tensor
-    3. remember where which sequence starts in offsets
+    2. smash all sequences together into a list
     """
-    label_list, seq_list, offsets = [], [], [0]
+    label_list, seq_list = [], []
     for (_label, _seq) in batch:
         label_list.append( _label )
         processed_seq = torch.tensor( _seq, dtype=torch.int32 )
         seq_list.append( processed_seq )
-        offsets.append( processed_seq.size(0) )
     label_list = torch.tensor( label_list, dtype=torch.float )
-    offsets = torch.tensor( offsets[:-1] ).cumsum( dim=0 ).type( torch.int32 )
-    seq_list = torch.cat( seq_list )
-    return label_list.to( device ), seq_list.to( device ), offsets.to( device )
+    return label_list.to( device ), seq_list
 
 
-def loss_batch(model, loss_func, labels, seqs, offsets, opt=None):
+def loss_batch(model, loss_func, labels, seqs, opt=None):
     labels = labels.unsqueeze(1)
-    loss = loss_func(model(seqs, offsets), labels)
+    loss = loss_func( model( seqs ), labels )
 
     if opt is not None:
         loss.backward()
         opt.step()
         opt.zero_grad()
 
-    return loss.item(), len(labels)
+    return loss.item(), len( labels )
 
 
 def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
     for epoch in range(epochs):
         model.train()
-        for labels, seqs, offsets in train_dl:
-            loss_batch(model, loss_func, labels, seqs, offsets, opt)
+        for labels, seqs in train_dl:
+            loss_batch(model, loss_func, labels, seqs, opt)
 
         model.eval()
         with torch.no_grad():
             losses, nums = zip(
-                *[loss_batch(model, loss_func, labels, seqs, offsets) for labels, seqs, offsets in valid_dl]
+                *[loss_batch( model, loss_func, labels, seqs ) for labels, seqs in valid_dl]
             )
-            val_loss = np.sum( np.multiply(losses, nums) ) / np.sum( nums )
+            val_loss = np.sum( np.multiply( losses, nums ) ) / np.sum( nums )
 
         print( epoch, val_loss )
 
 
-def get_model(stimuli_dim, hidden_dim, embedding_dim, lr):
-    model = SequenceClassificationModel( stimuli_dim, hidden_dim, embedding_dim )
-    return model, optim.SGD(model.parameters(), lr=lr)
+def get_model(input_dim, hidden_dim, embedding_dim, lr):
+    model = SequenceClassificationModel( input_dim, hidden_dim, embedding_dim )
+    return model, optim.SGD( model.parameters(), lr=lr )
 
 
 def get_data(train_ds, valid_ds, bs):
@@ -121,6 +121,25 @@ def get_trainstimuliSequence():
     ]
 
 
+def get_trainstimuliExtendedSequence():
+    return [
+        ( 1, ['A','C','F'], ),
+        ( 1, ['A','C','F','C','G'], ),
+        ( 1, ['A','C','G','F'], ),
+        ( 1, ['A','C','G','F','C','G'], ),
+        ( 1, ['A','D','C','F'], ),
+        ( 1, ['A','D','C','F','C'], ),
+        ( 1, ['A','D','C','F','C','G'], ),
+        ( 1, ['A','D','C','G','F','C','G'], ),
+        ( 0, ['A','C','F','G'] ),
+        ( 0, ['A','D','G','F','C'] ),
+        ( 0, ['A','C','C','G'] ),
+        ( 0, ['A','G','F','C','G'] ),
+        ( 0, ['A','D','C','F','G'] ),
+        ( 0, ['A','D','C','G','F','G','C'], ),
+    ]
+
+
 def get_teststimuliSequence():
     return [
         ( 1, ['A','C','F','C','G'], ),
@@ -140,15 +159,17 @@ def get_teststimuliSequence():
 def main():
     bs = 3
     ggen = GrammarGen()
-    train_ds = SequenceDataset( ggen.stim2seqs( get_trainstimuliSequence() ) )
-    valid_ds = SequenceDataset( ggen.generate( 12 ) )
+    seqs = ggen.stim2seqs( get_trainstimuliExtendedSequence() )
+    train_ds = SequenceDataset( seqs )
+    valid_ds = SequenceDataset( seqs )
     train_dl, valid_dl = get_data( train_ds, valid_ds, bs )
 
-    lr = 0.1
-    emsize = 5
-    stimuli_dim = len( ggen ) + 1
+    lr = 0.9
+    hidden_dim = 5
+    input_dim = len( ggen ) + 1
+    embedding_dim = 4
 
-    model, opt = get_model( stimuli_dim, 0, emsize, lr )
+    model, opt = get_model( input_dim, hidden_dim, embedding_dim, lr )
 
     loss_func = nn.BCELoss()
 
@@ -162,11 +183,13 @@ def main():
     model.eval()
     with torch.no_grad():
         losses, nums = zip(
-            *[loss_batch(model, loss_func, labels, seqs, offsets) for labels, seqs, offsets in test_dl]
+            *[loss_batch( model, loss_func, labels, seqs ) for labels, seqs in test_dl]
         )
         test_loss = np.sum( np.multiply(losses, nums) ) / np.sum( nums )
-        for labels, seqs, offsets in test_dl:
-            print( f'model: {model(seqs, offsets)} label: {labels} ')
+        for labels, seqs in test_dl:
+            preds = model( seqs )
+            for i in range( labels.size()[0] ):
+                print( f'seq: {seqs[i].tolist()} model: {preds[i]} label: {labels[i]} ')
 
     print( "Total loss: ", test_loss )
 
